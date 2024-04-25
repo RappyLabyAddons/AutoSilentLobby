49f011aa6b93318ecfef4700524681c71569f415
RappyTV <rappytv@rappytv.com>
2024-04-25 18:32:17 +0200

    Add getter for versioned api

--- core/src/main/java/com/rappytv/autosilentlobby/AutoSilentLobbyAddon.java
+++ core/src/main/java/com/rappytv/autosilentlobby/AutoSilentLobbyAddon.java
@@ -1,18 +1,27 @@
 package com.rappytv.autosilentlobby;
 
+import com.rappytv.autosilentlobby.api.IHotbarApi;
+import com.rappytv.autosilentlobby.core.generated.DefaultReferenceStorage;
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.models.addon.annotation.AddonMain;
 
 @AddonMain
 public class AutoSilentLobbyAddon extends LabyAddon<AutoSilentLobbyConfig> {
 
+    private IHotbarApi api;
+
     @Override
     protected void enable() {
         registerSettingCategory();
+        api = ((DefaultReferenceStorage) referenceStorageAccessor()).iHotbarApi();
     }
 
     @Override
     protected Class<? extends AutoSilentLobbyConfig> configurationClass() {
         return AutoSilentLobbyConfig.class;
+    }
+
+    public IHotbarApi getApi() {
+        return api;
     }
 }
