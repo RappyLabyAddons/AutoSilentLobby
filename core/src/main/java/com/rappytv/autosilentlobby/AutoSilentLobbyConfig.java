5fd07201ef53734d863740746cbc2c7b1555281b
RappyTV <rappytv@rappytv.com>
2024-04-25 18:18:10 +0200

    Add server config option

--- core/src/main/java/com/rappytv/autosilentlobby/AutoSilentLobbyConfig.java
+++ core/src/main/java/com/rappytv/autosilentlobby/AutoSilentLobbyConfig.java
@@ -2,15 +2,23 @@ package com.rappytv.autosilentlobby;
 
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
+import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.TextFieldSetting;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
+import java.util.ArrayList;
 
 public class AutoSilentLobbyConfig extends AddonConfig {
 
     @SwitchSetting
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
+    @TextFieldSetting
+    private final ConfigProperty<String> servers = new ConfigProperty<>("");
+
     @Override
     public ConfigProperty<Boolean> enabled() {
         return enabled;
+    }
+    public String[] servers() {
+        return servers.get().split(",");
     }
 }
