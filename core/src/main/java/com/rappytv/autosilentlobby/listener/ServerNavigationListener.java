45f5b2e50e4d687211cc9913269fd1f8a87e3bfd
RappyTV <rappytv@rappytv.com>
2024-04-26 12:17:11 +0200

    Fix left clicking

--- core/src/main/java/com/rappytv/autosilentlobby/listener/ServerNavigationListener.java
+++ core/src/main/java/com/rappytv/autosilentlobby/listener/ServerNavigationListener.java
@@ -3,15 +3,17 @@ package com.rappytv.autosilentlobby.listener;
 import com.rappytv.autosilentlobby.AutoSilentLobbyAddon;
 import com.rappytv.autosilentlobby.AutoSilentLobbyConfig;
 import com.rappytv.autosilentlobby.api.IHotbarApi;
-import net.labymod.api.Laby;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.network.server.ServerJoinEvent;
 import net.labymod.api.event.client.network.server.SubServerSwitchEvent;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ServerNavigationListener {
 
     private final AutoSilentLobbyConfig config;
     private final IHotbarApi api;
+    private final Timer timer = new Timer();
 
     public ServerNavigationListener(AutoSilentLobbyAddon addon) {
         this.config = addon.configuration();
@@ -31,9 +33,12 @@ public class ServerNavigationListener {
     }
 
     private void silentLobby() {
-        Laby.labyAPI().minecraft().executeNextTick(() -> {
-            api.changeSlot(config.slot() - 1);
-            api.click(config.clickType());
-        });
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                api.changeSlot(config.slot() - 1);
+                api.click(config.clickType());
+            }
+        }, 200);
     }
 }
